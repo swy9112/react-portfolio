6302dac6f3047ebf6ef2b040b06c37df48854303
seungwoo lee2 <seungwoo_lee2@tmax.co.kr>
2022-08-04 16:28:06 +0900

    update

--- src/components/Section.js
+++ src/components/Section.js
@@ -8,7 +8,12 @@ export const Wrap = styled.div`
   justify-content: ${(props) => (props.direction === 'column' ? 'center' : 'baseline')};
   width: 100%;
   height: 100vh;
+  padding: 60px 0;
   border-bottom: 1px solid #323232;
+
+  @media screen and (max-width: 1024px) {
+    height: auto;
+  }
 `;
 
 function Section({ children, direction = 'row' }) {
